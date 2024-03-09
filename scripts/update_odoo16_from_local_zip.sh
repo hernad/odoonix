1d26921008a765580ca553604dedeacc98c7d02c
Ernad Husremovic <hernad@bring.out.ba>
2024-03-09 15:20:43 +0100

    odoo-data sacuvati

--- scripts/update_odoo16_from_local_zip.sh
+++ scripts/update_odoo16_from_local_zip.sh
@@ -14,8 +14,14 @@ fi
 DIR_ZIP=$ODOO16_ZIP
 [ -d "$DIR_ZIP" ] && echo rm "$DIR_ZIP" && rm -rf $DIR_ZIP
 
+rm -rf tmp-odoo-data/*
+mkdir -p tmp-odoo-data
+cp -av $ODOO16/odoo-data tmp-odoo-data/
+
 DIR=$ODOO16
 [ -d "$DIR" ] && echo "rm $DIR" & rm -rf $DIR
+
+
 
 unzip ${ODOO16_ZIP}.zip
 
@@ -26,6 +32,9 @@ echo "get from git $ODOO16"
 git checkout -- $ODOO16
 
 scripts/remove_unused_prevodi.sh
+
+echo vrati postojeci $ODOO16/odoo-data
+cp -av tmp-odoo-data/odoo-data $ODOO16/
 
 ls $ODOO16/odoo/addons/base/i18n/
 
