87f0ebe2b3d5af5e17479e089fe62ef117aac90b
Ernad Husremovic <hernad@bring.out.ba>
2024-03-13 16:47:40 +0100

    clean nix shell

--- scripts/nix_dev_shell_odoo.sh
+++ scripts/nix_dev_shell_odoo.sh
@@ -13,13 +13,13 @@ echo " "
 
 # https://github.com/NixOS/nix/issues/6091
 
-#XDG_DATA_DIRS_SAVED=$XDG_DATA_DIRS
+XDG_DATA_DIRS_SAVED=$XDG_DATA_DIRS
 
-#echo "NOTICE:"
-#echo "running nix develop ..."
-#echo "after entering into nix develop shell, type exit to continue"
-#echo "------------------------------------------------------------------------"
-#nix develop
+echo "NOTICE:"
+echo "running nix develop ..."
+echo "after entering into nix develop shell, type exit to continue"
+echo "------------------------------------------------------------------------"
+nix develop
 
 echo "idem u odoo16 i pokrecem vscode"
 
@@ -30,6 +30,6 @@ nix shell .#nginx-start \
           nixpkgs#pkgs.unzip nixpkgs#pkgs.zip \
           nixpkgs#pkgs.bashInteractive --command sh -c "cd odoo16 && code ."
 
-#XDG_DATA_DIRS=$XDG_DATA_DIRS_SAVED
+XDG_DATA_DIRS=$XDG_DATA_DIRS_SAVED
 
 #. <(nix print-dev-env)
