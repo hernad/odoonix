57e9ec8231607f8327e4c36925d85f1778560073
Ernad Husremovic <hernad@bring.out.ba>
2024-03-13 21:19:42 +0100

    odooMat

---
+++ odoo16/addons/bringout/odoo_mat/__manifest__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Author: Ernad Husremovic
+# mail:   hernad@bring.out.ba
+{
+    "name": "Bosnia and Herzegovina",
+    "description": """
+odoo materijalno
+""",
+    "version": "16.0.0",
+    "author": "bring.out",
+    'category': 'Localization',
+    "website": "https://github.com/hernad/odoo",
+    'depends': [
+        "l10n_bs",
+        "account",
+        "product",
+        "om_account_accountant",
+        "l10n_generic_coa",
+        "analytic",
+        "mrp",
+        "mrp_account",
+        "mrp_landed_costs",
+        "mrp_subcontracting",
+        "mrp_subcontracting_account",
+        "mrp_subcontracting_purchase",
+        "mrp_subonctracting_landed_costs",
+        "stock",
+        "barcodes",
+        "barcodes_gs1_nomenclature",
+        "stock_account",
+        "stock_landed_costs",
+        #"stock_picking_batch",
+        "purchase",
+        "purchase_mrp",
+        "sale_management",
+        "sale_mrp",
+        "sale_project",
+        "sale_project_stock",
+        "uom",
+        "product_print_zpl_barcode",
+        "product_multiple_barcodes",
+        "project",
+        "project_mrp",
+        "project_purchase",
+        "project_sale_expense",
+        "maintenance"
+    ],
+    'data': [
+    ],
+    "active": False,
+    'license': 'MIT',
+}
