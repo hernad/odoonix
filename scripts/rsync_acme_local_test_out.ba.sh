9f7421e458c3c51781cecc290aa273d959a345a4
Ernad Husremovic <hernad@bring.out.ba>
2024-05-06 16:35:46 +0200

    rsync local.test.out.ba

--- scripts/rsync_acme_local_test_out.ba.sh
+++ scripts/rsync_acme_local_test_out.ba.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -avz root@router-5:/var/lib/acme/local.test.out.ba/   /data/odoo/odoo-mat/acme/local.test.out.ba
+rsync -avz root@router-5:/var/lib/acme/local.test.out.ba/   /data/odoo/acme/local.test.out.ba
