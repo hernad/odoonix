c4fb43658b737f8cff0eef77bb896b45fd0c2c4d
Ernad Husremovic <hernad@bring.out.ba>
2024-05-06 19:15:08 +0200

    addons

---
+++ odoo16/scripts/zip_upload_addons.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd 3p_addons
+
+for ADDON in akretion bringout cybrosys OCA odoomates ventor
+do
+
+  ZIP_NAME=odoo_16_${ADDON}_`date +"%Y%m%d"`
+  echo $ZIP_NAME
+
+  
+  zip -r ../${ZIP_NAME}.zip --exclude="./.git/*/*" --exclude="./3p_addons/*/*" \
+       --exclude="./odoo-data/*/*" --exclude="./scripts/*/*"  $ADDON/*
+
+  rsync -avz ../${ZIP_NAME}.zip root@download.svc.bring.out.ba:/data/download/
+done
+
+cd ..
+
+ssh root@download.svc.bring.out.ba ls -lh /data/download
