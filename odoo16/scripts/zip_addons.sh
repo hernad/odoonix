0959f7507f6e86dca9df215b6cb7726bc1313f27
Ernad Husremovic <hernad@bring.out.ba>
2024-09-04 18:35:46 +0200

    zip only OCA modules

--- odoo16/scripts/zip_addons.sh
+++ odoo16/scripts/zip_addons.sh
@@ -17,8 +17,16 @@ do
   ZIP_NAME=odoo_16_${ADDON}_`date +"%Y%m%d"`
   echo $ZIP_NAME
 
+  zip -r ../${ZIP_NAME}.zip --exclude="./.git/*/*" --exclude="./3p_addons/*/*" \
+       --exclude="./odoo-data/*/*" --exclude="./*.zip"  --exclude="./scripts/*/*"  $ADDON/*
+
+
 done
 
 cd ..
 
-ssh root@download.svc.bring.out.ba ls -lh /data/download
+ls -l $ZIP_NAME.zip
+echo "pokreni komandu:"
+echo "rsync -avz ${ZIP_NAME}.zip root@download.svc.bring.out.ba:/data/download/"
+
+#ssh root@download.svc.bring.out.ba ls -lh /data/download
