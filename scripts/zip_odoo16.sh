6c0ddc0252f27190f66ed61623ab97d31a0a6067
Ernad Husremovic <hernad@bring.out.ba>
2024-10-23 13:09:47 +0200

    zip_odoo16.sh

--- scripts/zip_odoo16.sh
+++ scripts/zip_odoo16.sh
@@ -24,6 +24,9 @@ zip -r $ZIP_NAME.zip --exclude="./.git/*/*"  \
    --exclude="./*/odoo.conf" \
     odoo16
 
-rsync -avz ${ZIP_NAME}.zip root@download.svc.bring.out.ba:/data/download/
+if [ "$1" == "--upload" ] ; then
 
-ssh root@download.svc.bring.out.ba ls -lh /data/download
+  rsync -avz ${ZIP_NAME}.zip root@download.svc.bring.out.ba:/data/download/
+  ssh root@download.svc.bring.out.ba ls -lh /data/download
+
+fi
